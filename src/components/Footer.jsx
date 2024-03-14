326c0e7437b92810dc36afdbeb8e72afcb14fc36
Sean <seanang123456@gmail.com>
2024-03-14 17:48:46 +0800

    Added Clickable address in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,11 @@ function Footer() {
                                 <Typography sx={{ fontSize: '0.9rem', mb: 1 }}>Contact Us</Typography>
                                 <Container sx={{ display: 'flex', textAlign: 'center', justifyContent: 'center', mb: 1 }}>
                                     <LocationOn sx={{ fontSize: '0.9rem', mr: 0.5 }} />
-                                    <Typography sx={{ fontSize: '0.7rem' }}>10 Winstedt Rd, #03-30A, Singapore 227977</Typography>
+                                    <Typography sx={{ fontSize: '0.7rem' }}>
+                                        <a href="https://www.google.com/maps/place/Gemen+Pte+Ltd/@1.3102084,103.8389323,17z/data=!3m2!4b1!5s0x31da19fc08c4d9fb:0x4f3412b3621c50dc!4m6!3m5!1s0x31da19461c78640f:0xed591bfcac18c41f!8m2!3d1.310203!4d103.8415072!16s%2Fg%2F11fct8jl5g?entry=ttu">
+                                            10 Winstedt Rd, #03-30A, Singapore 227977
+                                        </a>
+                                    </Typography>
                                 </Container>
                                 <Container sx={{ display: 'flex', textAlign: 'center', justifyContent: 'center', mb: 1 }}>
                                     <Email sx={{ fontSize: '0.9rem', mr: 0.5 }} />
