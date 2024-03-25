3658b4614489a6aea0096842124e5b91104134ff
Sean <seanang123456@gmail.com>
2024-03-25 09:04:46 +0800

    Minor changes to navbar item name

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
                 <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', textAlign: 'center' }}>
 
                   <Link to="/homepage#aboutus-section" sx={{ mr: 2, ml: 2 }}>
-                    <Typography>About Us</Typography>
+                    <Typography>Why Us</Typography>
                   </Link>
 
                   <Button
